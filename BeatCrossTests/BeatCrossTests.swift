0616933ce68a3bf3b20f66a9e4d3c683641e07dc
Youta <0x59.xxx@gmail.com>
2024-12-29 13:19:39 +0900

    Initial Commit

---
+++ BeatCrossTests/BeatCrossTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeatCrossTests.swift
+//  BeatCrossTests
+//
+//  Created by X on 2024/12/29.
+//
+
+import Testing
+@testable import BeatCross
+
+struct BeatCrossTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
