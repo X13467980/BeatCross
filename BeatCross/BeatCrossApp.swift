2585c2f2c968994177fca001877427d65c5c28a2
Youta <0x59.xxx@gmail.com>
2025-02-05 00:43:15 +0900

    Add: replace BeatCrossApp

--- BeatCross/BeatCrossApp.swift
+++ BeatCross/BeatCrossApp.swift
@@ -2,16 +2,30 @@
 //  BeatCrossApp.swift
 //  BeatCross
 //
-//  Created by X on 2025/02/05.
+//  Created by X on 2024/12/29.
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct BeatCrossApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+      WindowGroup {
+        NavigationView {
+          WelcomeView()
         }
+      }
     }
-}
+  }
+ 
